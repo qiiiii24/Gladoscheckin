de07b96f9645c44b411fcbd95e27bde8378dfa1d
qiiiii24 <114066071+qiiiii24@users.noreply.github.com>
2025-06-10 21:54:02 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
                 # 获取账号当前状态
                 result = state.json()
                 # 获取剩余时间
-                leftdays = int(float(result['data']['leftDays']))
+                # leftdays = int(float(result['data']['leftDays']))
                 # 获取账号email
                 email = result['data']['email']
                 
